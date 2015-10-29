6c27841774f1130cf98782b39f4c286cbdf369d3
Aldo Zaimi <aldo.zaimi@polymtl.ca>
2015-10-29 19:26:30 -0400

    function to visualize all relations between stats parameters for true vs false axons

--- code/visualize_DiscrAnalysis.m
+++ code/visualize_DiscrAnalysis.m
@@ -3,12 +3,43 @@ function visualize_DiscrAnalysis(classifier,var_names)
 X=classifier.X;
 Y=classifier.Y;
 
-figure;
-gplotmatrix(X,X,Y);
-xlabel(var_names);
-ylabel(var_names);
-% text([.08 .24 .43 .66 .83], repmat(-.1,1,5), var_names);
-% text(repmat(-.12,1,5), [.86 .62 .41 .25 .02], var_names, 'FontSize',8, 'Rotation',90);
+K = classifier.Coeffs(1,2).Const; 
+L = classifier.Coeffs(1,2).Linear;
+
+% Const + Linear * x + x' * Quadratic * x = 0,
+% 
 
 
+% % Plot the curve K + [x,y]*L  = 0.
+% f = @(x1,x2,x3,x4,x5,x6,x7,x8,x9) K + L(1)*x1 + L(2)*x2+ L(3)*x3+ L(4)*x4+ L(5)*x5+ L(6)*x6+ L(7)*x7+ L(8)*x8+ L(9)*x9;
+
+
+
+
+figure(1);
+[h,ax,bigax] = gplotmatrix(X,X,Y);
+
+for i=1:size(var_names,1)    
+    xlabel(ax(9,i),var_names{i});
+    ylabel(ax(i,1),var_names{i});
+end
+
+
+
+
+% hold on;
+% 
+% for i=1:size(var_names,1)
+%     ezplot(f, ax(1,1));
+%     xlabel(ax(9,i),var_names{i});
+%     ylabel(ax(i,1),var_names{i});
+% end
+
+% % Plot the curve K + [x,y]*L  = 0.
+figure(2);
+f = @(x1,x2) K + L(1)*x1 + L(2)*x2;
+h2 = ezplot(f,[0 200 0 150]);
+h2.Color = 'blue';
+h2.LineWidth = 2;
+% hold off;
 
