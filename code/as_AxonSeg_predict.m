42e194289bba46beede6588700b455d62107f286
Aldo Zaimi <aldo.zaimi@polymtl.ca>
2015-11-04 18:25:53 -0500

    function that uses a discriminant analysis object to predict class for a new set of values

---
+++ code/as_AxonSeg_predict.m
@@ -0,0 +1,40 @@
+function AxonSeg_DA = as_AxonSeg_predict(AxSeg,classifier_final, parameters)
+% Ex : AxonSeg_DA = as_AxonSeg_predict(AxSeg,classifier_final, parameters);
+
+
+% Calculate stats of the image
+
+[Stats_3, names3] = as_stats_axons(AxSeg);
+Stats_3_used = rmfield(Stats_3,setdiff(names3, parameters));
+
+% Predict true & false axons by using the classifier (INPUT)
+
+Stats_3_used = table2array(struct2table(Stats_3_used));
+[label,score,~] = predict(classifier_final,Stats_3_used);
+
+% Identify true & false axons 
+
+index1=find(label==1);
+Rejected_axons_img = ismember(bwlabel(AxSeg),index1);
+
+index2=find(label==2);
+Accepted_axons_img = ismember(bwlabel(AxSeg),index2);
+
+AxonSeg_DA = Accepted_axons_img;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
