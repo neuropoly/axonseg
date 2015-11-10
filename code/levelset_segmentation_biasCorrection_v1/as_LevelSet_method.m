2e9e083145434e3b8c84705a06a0ac719f75d7d5
Aldo Zaimi <aldo.zaimi@polymtl.ca>
2015-11-10 17:27:57 -0500

    updated LevelSet function

--- code/levelset_segmentation_biasCorrection_v1/as_LevelSet_method.m
+++ code/levelset_segmentation_biasCorrection_v1/as_LevelSet_method.m
@@ -1,55 +1,57 @@
 function img = as_LevelSet_method(img_init)
-% 
-% close all;
-% clear all;
+% Uses the Level Set method to compute axon segmentation
+
+% Some initial operations
 
 Img=img_init;
 Img=double(Img(:,:,1)); % Convert initial image to 2 dim double
+
 A=255;
 Img=A*normalize01(Img); % normalize intensities from 0 to 254
-nu=0.001*A^2; % coefficient of arc length term
 
+% *** SET VARIOUS PARAMETERS HERE *****************************************
+
+nu=0.001*A^2; % coefficient of arc length term
 sigma = 4; % scale parameter that specifies the size of the neighborhood
-iter_outer=30; 
+
+iter_outer=30;   % number of iterations to compute
 iter_inner=10;   % inner iteration for level set evolution
 
 timestep=.1;
 mu=1;  % coefficient for distance regularization term (regularize the level set function)
 
 c0=1;
-figure(1);
-% imagesc(Img,[0, 255]); colormap(gray); axis off; axis equal
+
+epsilon=1;
+b=ones(size(Img));  %%% initialize bias field with 1s same size as image
+
+
+
+% *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+
 
 % initialize level set function
 
-
 initialLSF = c0*ones(size(Img));  % a matrix of 1s with same size as Img
+initialLSF(5:size(Img,1)-2,5:size(Img,2)-2) = -c0; % Put square of (-1s) near the borders of the image to segment
 
 
-
-initialLSF(3:20,3:20) = -c0; % Put square of (-1s) in the middle of initialLSF
-%initialLSF(30:90,50:90) = -c0; % Put square of (-1s) in the middle of initialLSF
-% initialLSF(5:size(Img,1)-2,5:size(Img,2)-2) = -c0; % Put square of (-1s) in the middle of initialLSF
-
+% initialLSF(3:20,3:20) = -c0; % Put square of (-1s) in the middle of initialLSF
+% %initialLSF(30:90,50:90) = -c0; % Put square of (-1s) in the middle of initialLSF
 % cercle = strel('disk',20);
 
 
 u=initialLSF; % Our u will be the initialLSF (has the initial contour)
 
-
-
-hold on;
-contour(u,[0 0],'r');
-title('Initial contour');
+% hold on;
+% contour(u,[0 0],'r');
+% title('Initial contour');
 
 % figure(2);
 % imagesc(Img,[0, 255]); colormap(gray); axis off; axis equal
 % hold on;
 % contour(u,[0 0],'r');
 % title('Initial contour');
-
-epsilon=1;
-b=ones(size(Img));  %%% initialize bias field with 1s same size as image
 
 K=fspecial('gaussian',round(2*sigma)*2+1,sigma); % Gaussian kernel
 KI=conv2(Img,K,'same'); % Result of convolution of image Img by K gaussian kernel
@@ -60,12 +62,15 @@ N=row*col; % Number of pixels in image (product of x size x y size)
 
 
 
-
 for n=1:iter_outer
     
 tic    
 [u, b, C]= lse_bfe(u,Img, b, K,KONE, nu,timestep,mu,epsilon, iter_inner);    
 toc
+
+
+
+
 disp(n);
 
 
@@ -83,8 +88,8 @@ end
 
 
 
-Mask =(Img>10); % Create a binary mask
-Img_corrected=normalize01(Mask.*Img./(b+(b==0)))*255;
+% Mask =(Img>10); % Create a binary mask
+% Img_corrected=normalize01(Mask.*Img./(b+(b==0)))*255;
 
 % figure(3); imagesc(b);  colormap(gray); axis off; axis equal;
 % title('Bias field');
@@ -96,7 +101,11 @@ Img_corrected=normalize01(Mask.*Img./(b+(b==0)))*255;
 %imshow(imcomplement(u));
 
 % img = imcomplement(u);
-img = u;
+
+img=uint8(u);
+img=logical(img);
+img=imfill(img,'holes');
+img=bwmorph(img,'clean');
 
 
 
