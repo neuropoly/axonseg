95c3053c6996d817c88f8c7a8c7c1e1d65a8e633
Aldo Zaimi <aldo.zaimi@polymtl.ca>
2015-11-10 17:09:47 -0500

    modified find_cost function to have more control on the values calculated by adding another parameter + Sensitivity & Specificity vectors are now directly calculated here in added on output

--- code/find_cost.m
+++ code/find_cost.m
@@ -1,11 +1,27 @@
-function Cost = find_cost(classifier, Sensitivity)
+function [Cost,ROC_values] = find_cost(classifier, Sensitivity)
 % cost = find_cost(classifier,95);
 
-for i=1:300
+
+
+for i=10:300
 
     classifier.Cost(2,1)=i;
     R = confusionmat(classifier.Y,resubPredict(classifier));
     
+    [Stats_ROC]=ROC_calculate(R);
+    
+    if i==10
+    
+    Sensitivity_values=Stats_ROC(1);
+    Specificity_values=Stats_ROC(2);
+    
+    else
+        
+    Sensitivity_values=[Sensitivity_values;Stats_ROC(1)];
+    Specificity_values=[Specificity_values;Stats_ROC(2)];
+        
+    end
+   
     FN_test = R(2,1);
     
     if FN_test <= round((R(2,2)/Sensitivity)-R(2,2))
@@ -24,5 +40,9 @@ for i=1:300
 end
 
 if i==300
-    Cost=1;
+    Cost=10;
 end
+
+
+
+ROC_values=[Sensitivity_values,Specificity_values];
