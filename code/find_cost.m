d5972fcacf160c5c6c374f7fb6646d6862707d38
Aldo Zaimi <aldo.zaimi@polymtl.ca>
2015-11-11 18:38:54 -0500

    some small changes (we will use Balanced Accuracy as a metric in order to select the best ROC curve position)

--- code/find_cost.m
+++ code/find_cost.m
@@ -1,27 +1,39 @@
 function [Cost,ROC_values] = find_cost(classifier, Sensitivity)
 % cost = find_cost(classifier,95);
 
-
+    Sensitivity_values=0;
+    Specificity_values=1;
 
 for i=10:300
 
+    
+
+    
+    
+    
+    
     classifier.Cost(2,1)=i;
     R = confusionmat(classifier.Y,resubPredict(classifier));
     
     [Stats_ROC]=ROC_calculate(R);
     
+    
     if i==10
-    
-    Sensitivity_values=Stats_ROC(1);
-    Specificity_values=Stats_ROC(2);
-    
-    else
         
-    Sensitivity_values=[Sensitivity_values;Stats_ROC(1)];
-    Specificity_values=[Specificity_values;Stats_ROC(2)];
+            Precision_values=Stats_ROC(3);
+            Accuracy_values=Stats_ROC(4);
+            Balanced_accuracy_values=Stats_ROC(5);
         
     end
-   
+    
+    
+    
+    Sensitivity_values=[Sensitivity_values;Stats_ROC(1)];
+    Specificity_values=[Specificity_values;Stats_ROC(2)];
+    Precision_values=[Precision_values;Stats_ROC(3)];
+    Accuracy_values=[Accuracy_values;Stats_ROC(4)];
+    Balanced_accuracy_values=[Balanced_accuracy_values;Stats_ROC(5)];
+        
     FN_test = R(2,1);
     
     if FN_test <= round((R(2,2)/Sensitivity)-R(2,2))
@@ -42,6 +54,10 @@ end
 if i==300
     Cost=10;
 end
+
+
+    Sensitivity_values=[Sensitivity_values;1];
+    Specificity_values=[Specificity_values;0];
 
 
 
