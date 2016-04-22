ad0fd117c3076204413d63b29c4da3da241e08f8
Aldo Zaimi <aldo.zaimi@polymtl.ca>
2016-04-22 16:10:56 -0400

    update bug as_downsample_2nii

--- code/Stats/as_stats_downsample_2nii.m
+++ code/Stats/as_stats_downsample_2nii.m
@@ -6,9 +6,9 @@ function [stats_downsample, statsname]=as_stats_downsample_2nii(axonlist,matrixs
 % EXAMPLE: as_stats_downsample_2nii(axonlist,size(img),PixelSize,150)
 
 [stats_downsample, statsname]=as_stats_downsample(axonlist,matrixsize,PixelSize,resolution);
-imagesc(stats_downsample(:,:,end),[3 6])
+imagesc(stats_downsample(:,:,end))
 mkdir('stats')
-save_nii_v2(make_nii(permute(stats_downsample,[1 2 4 3]),[150 150 1]),'stats/stats_downsample4D.nii');
+save_nii(make_nii(permute(stats_downsample,[1 2 4 3]),[150 150 1]),'stats/stats_downsample4D.nii');
 for istat=1:length(statsname)
-    save_nii_v2(make_nii(permute(stats_downsample(:,:,istat),[1 2 4 3]),[150 150 1]),['stats/' num2str(istat) '_' statsname{istat}]);
+    save_nii(make_nii(permute(stats_downsample(:,:,istat),[1 2 4 3]),[150 150 1]),['stats/' num2str(istat) '_' statsname{istat} '.nii']);
 end
