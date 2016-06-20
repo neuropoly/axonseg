ded28fe4310f7edcfeb7cbfb0d2957ccc9714c6e
Aldo Zaimi <aldo.zaimi@polymtl.ca>
2016-06-20 18:39:02 -0400

    added function for estimation of gap between axons

---
+++ code/gap_axons.m
@@ -0,0 +1,47 @@
+
+
+function [mean_gap_axon]=gap_axons(axonlist,nbr_of_neighbours)
+
+
+for i=1:size(cat(1,axonlist.Centroid),1)
+    
+origin_centroid=axonlist(i).Centroid;
+
+for j=1:size(cat(1,axonlist.Centroid),1)
+     
+X=[origin_centroid;axonlist(j).Centroid];
+dist_vector(j,1)=j;
+dist_vector(j,2)=pdist(X,'euclidean');
+dist_vector(j,2)=dist_vector(j,2)*PixelSize;
+
+end   
+    
+    
+
+Sorted_distances=sortrows(dist_vector,2);    
+    
+neighbour_axons=Sorted_distances(1:nbr_of_neighbours,:);
+
+
+for k=2:size(neighbour_axons,1)
+
+% tmp=axonlist(neighbour_axons(k)).Centroid;
+gap_axon(k-1,1)=neighbour_axons(k,2)-0.5*(axonlist(i).axonEquivDiameter)-axonlist(i).myelinThickness...
+    -0.5*(axonlist(neighbour_axons(k)).axonEquivDiameter) - axonlist(neighbour_axons(k)).myelinThickness;
+
+gap_axon(gap_axon<0)=0;
+
+end
+
+mean_gap_axon(i,1)=mean(gap_axon);
+
+
+end
+
+
+
+
+
+
+
+
