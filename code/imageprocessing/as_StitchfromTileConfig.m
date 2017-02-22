aac88a58458ff071c217207c4b7263ecbd5f7ea6
Tanguy Duval <duvaltanguy@gmail.com>
2017-02-22 17:43:05 -0500

    bug to stitch png with 2D images

--- code/imageprocessing/as_StitchfromTileConfig.m
+++ code/imageprocessing/as_StitchfromTileConfig.m
@@ -13,9 +13,10 @@ Msize = size(imread(fname{1}));
 RowPos = round(RowPos-min(RowPos));
 ColPos = round(ColPos-min(ColPos));
 
-Panorama = uint8(round(max(RowPos)+Msize(1)+1),round(max(RowPos)+Msize(2)+1));
+Panorama = uint8(zeros(round(max(RowPos)+Msize(1)+1),round(max(RowPos)+Msize(2)+1)));
 for ff = 1:length(fname)
-    Panorama((RowPos(ff)+1):(RowPos(ff)+Msize(1)),(ColPos(ff)+1):(ColPos(ff)+Msize(2)))=rgb2gray(imread(fname{ff}));
+    tmp = imread(fname{ff}); if size(tmp,3)==3, tmp = rgb2gray(tmp); end
+    Panorama((RowPos(ff)+1):(RowPos(ff)+Msize(1)),(ColPos(ff)+1):(ColPos(ff)+Msize(2)))=tmp;
 end
 
 as_display_LargeImage(Panorama);
